0b6df9395858216923c41f33ad52839206d695d8
tsurumi822 <nozomi.tsurumi@divx.co.jp>
2023-07-06 12:35:02 +0900

    サンプルQRコード生成

--- app/controllers/qrcodes_controller.rb
+++ app/controllers/qrcodes_controller.rb
@@ -1,9 +1,29 @@
 require 'rqrcode'
+require 'chunky_png'
 
 class QrcodesController < ApplicationController
   def generate
-    qrcode = RQRCode::QRCode.new('Hello, QR code!')
-    svg = qrcode.as_svg
-    send_data svg, type: 'image/svg+xml', disposition: 'inline'
+    data = '0292000100100000012249520230725202307281'
+
+    # QRコードを生成
+    qrcode = RQRCode::QRCode.new(data, :size => 4, :level => :q)
+    
+    # QRコードをイメージ画像に変換
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      module_px_size: 6,
+      resize_exactly_to: false
+    )
+
+    # イメージ画像をデスクトップに保存
+    desktop_path = File.expand_path('~/Desktop')
+    file_name = "#{URI.encode_www_form_component(data)}.png"
+    file_path = File.join(desktop_path, file_name)
+    png.save(file_path)
+
+    # ブラウザにイメージ画像を表示
+    send_data File.read(file_path), type: 'image/png', disposition: 'inline'
   end
 end
